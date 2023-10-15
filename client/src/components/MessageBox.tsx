1a63ada2c0b683163038038cce3294621624d2bc
NhanPhan159 <nhandut2019@gmail.com>
2023-10-15 14:11:47 +0700

    FE: detail trip/ fix UI

--- client/src/components/MessageBox.tsx
+++ client/src/components/MessageBox.tsx
@@ -1,51 +1,20 @@
-import data from "@emoji-mart/data";
-import Picker from "@emoji-mart/react";
-import { useState } from "react";
-import { Button } from "primereact/button";
-import {BsEmojiSmile} from "react-icons/bs"
-import {GrAttachment} from "react-icons/gr"
-
+import data from "../fakeData";
+import TextType from "./MessageItem"
 const MessageBox = () => {
-    const [visibleEmoji, setVisibleEmoji] = useState(false);
-    const [message, setMessage] = useState("");
     return ( 
-        <div className="flex p-4 gap-4 bg-white items-center border-x border-slate-100">
-            <div>
-                <div className={visibleEmoji ? "absolute bottom-16" : "hidden"}>
-                    <Picker
-                        data={data}
-                        emojiSize={20}
-                        emojiButtonSize={30}
-                        previewPosition="none"
-                        onEmojiSelect={(e: any) => {
-                        setVisibleEmoji(!visibleEmoji);
-                        setMessage(message + e.native);
-                        }}
-                    />
-                </div>
-                <BsEmojiSmile
-                className="w-6 h-6 hover:bg-gray-500 hover:text-white focus:ring-4 font-medium rounded-full text-base text-center inline-flex items-center"
-                onClick={() => {
-                    setVisibleEmoji(!visibleEmoji);
-                }}
-                />
-          </div>
-          <GrAttachment className="h-6 w-6"/>
-          <input
-            type="text"
-            id="first_name"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            className="bg-gray-100 flex-1 border border-gray-400 text-black text-sm rounded-full p-2.5 "
-            placeholder="Type a message ..."
-          />
-          <Button 
-            icon="pi pi-send" 
-            rounded 
-            outlined
-            onClick={()=>console.log(message)} />
+        <div className="p-5">
+            {data.map((e)=>{
+                switch(e.type){
+                    case "link":
+                        return <div></div>
+                    case "image":
+                        return <div></div>
+                    default:
+                        return <TextType params={e}/>
+                }
+            })}
         </div>
      );
 }
  
-export default MessageBox
+export default MessageBox;
